2bcce1b1708e0ef104cb28cd3dddae18c569e176
jgartee <jlgartee@gmail.com>
2019-04-24 21:55:05 -0400

    test 2 out-of-order elements.  add computation transform (or statement -> statements transform)

--- src/test/java/IntegerSort/SortTest.java
+++ src/test/java/IntegerSort/SortTest.java
@@ -5,6 +5,7 @@ package IntegerSort;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -25,9 +26,24 @@ public class SortTest {
     public void sortings() throws Exception {
         assertSorted(intList(), intList());
         assertSorted(intList(1), intList(1));
+        assertSorted(intList(2, 1), intList(1, 2));
     }
 
     private List<Integer> sort(List<Integer> list) {
-        return list;
+        List<Integer> sorted = new ArrayList<Integer>();
+
+        if (list.size() <= 1) {
+            return list;
+        }
+
+        if (list.get(0) > list.get(1)) {
+            sorted.add(list.get(1));
+            sorted.add(list.get(0));
+        } else {
+            sorted.add(list.get(0));
+            sorted.add(list.get(1));
+        }
+
+        return sorted;
     }
 }
