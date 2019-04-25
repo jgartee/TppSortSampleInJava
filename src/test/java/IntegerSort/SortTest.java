42d948664474e1382086cd0c362062ffb947ed42
jgartee <jlgartee@gmail.com>
2019-04-25 06:54:24 -0400

    test list of 3 where last two items out of order: statemeent -> stateements transform (computational)

--- src/test/java/IntegerSort/SortTest.java
+++ src/test/java/IntegerSort/SortTest.java
@@ -27,6 +27,7 @@ public class SortTest {
         assertSorted(intList(), intList());
         assertSorted(intList(1), intList(1));
         assertSorted(intList(2, 1), intList(1, 2));
+        assertSorted(intList(2, 1, 3), intList(1, 2, 3));
     }
 
     private List<Integer> sort(List<Integer> list) {
@@ -34,16 +35,25 @@ public class SortTest {
 
         if (list.size() <= 1) {
             return list;
+        } else if (list.size() == 2) {
+            if (list.get(0) > list.get(1)) {
+                sorted.add(list.get(1));
+                sorted.add(list.get(0));
+            } else {
+                sorted.add(list.get(0));
+                sorted.add(list.get(1));
+            }
+        } else if (list.size() == 3){
+            if (list.get(1) > list.get(2)) {
+                sorted.add(list.get(2));
+                sorted.add(list.get(0));
+                sorted.add(list.get(1));
+            } else {
+                sorted.add(list.get(1));
+                sorted.add(list.get(0));
+                sorted.add(list.get(2));
+            }
         }
-
-        if (list.get(0) > list.get(1)) {
-            sorted.add(list.get(1));
-            sorted.add(list.get(0));
-        } else {
-            sorted.add(list.get(0));
-            sorted.add(list.get(1));
-        }
-
-        return sorted;
+            return sorted;
     }
 }
