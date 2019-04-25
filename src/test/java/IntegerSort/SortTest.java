e6a1c7a86f514cb2a61f41e56af5ff3975c0f36b
jgartee <jlgartee@gmail.com>
2019-04-24 20:35:21 -0400

    add guard statement to ensure list size > 1: split flow transform

--- src/test/java/IntegerSort/SortTest.java
+++ src/test/java/IntegerSort/SortTest.java
@@ -26,15 +26,16 @@ public class SortTest {
     public void sortings() throws Exception {
         assertSorted(intList(), intList());
         assertSorted(intList(1), intList(1));
-        assertSorted(intList(2,1), intList(1,2));
+        assertSorted(intList(2, 1), intList(1, 2));
     }
 
     private List<Integer> sort(List<Integer> list) {
-
-        if(list.get(0) > list.get(1)){
-            Integer temp = list.get(0);
-            list.set(0, list.get(1));
-            list.set(1, temp);
+        if (list.size() > 1) {
+            if (list.get(0) > list.get(1)) {
+                Integer temp = list.get(0);
+                list.set(0, list.get(1));
+                list.set(1, temp);
+            }
         }
 
         return list;
