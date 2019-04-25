6fe00b9e1ebf3084d99156cfd3c3bbdbd54c6083
jgartee <jlgartee@gmail.com>
2019-04-24 21:27:24 -0400

    refactor & cleanup

--- src/test/java/IntegerSort/SortTest.java
+++ src/test/java/IntegerSort/SortTest.java
@@ -32,12 +32,10 @@ public class SortTest {
     }
 
     private List<Integer> sort(List<Integer> list) {
-        for (int size = list.size(); size > 0; size--) {
-            for (int index = 0; size > index + 1; index++) {
+        for (int size = list.size(); size > 0; size--)
+            for (int index = 0; size > index + 1; index++)
                 if (outOfOrder(list, index))
                     swap(list, index);
-            }
-        }
 
         return list;
     }
